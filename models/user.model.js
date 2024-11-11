191c14fc2bc1793cb41c4e9d3c2857a2f20aa5c6
Aman Islam <amanislam019@gmail.com>
2024-11-11 17:17:39 +0500

    Create user.model.js

---
+++ models/user.model.js
@@ -0,0 +1,50 @@
+"use strict";
+const moment = require("moment");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("users", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 128],
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+
+  User.beforeCreate((user) => {
+    user.dataValues.createdAt = moment().unix();
+    user.dataValues.updatedAt = moment().unix();
+  });
+  User.beforeUpdate((user) => {
+    user.dataValues.updatedAt = moment().unix();
+  });
+  
+  return User;
+};
